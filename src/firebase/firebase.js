40ef0e507c35aaa23a0d2e518758ec05c2f6087c
Maryana182 <111122257+Maryana182@users.noreply.github.com>
2022-11-07 10:18:37 +0200

    Init commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: "1:1034820492366:web:f06395930a0755dd8bb71f",
+    measurementId: "G-WTKSN06NF4"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
+
